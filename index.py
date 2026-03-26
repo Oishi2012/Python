3c169fbe244a4e43a1014daf578aa9ad55f170a8
Oishii2012 <memahmudaa@gmail.com>
2026-03-26 22:25:21 +0600

    My turtle

---
+++ index.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.Screen().bgcolor("Orange")
+sc=turtle.Screen()
+sc.setup(400,300)
+turtle.title('Welcome to turtle window')
+
+mypen=turtle.Turtle()
+mypen.forward(100)
+mypen.left(90)
+
+mypen.forward(100)
+mypen.left(90)
+
+mypen.forward(100)
+mypen.left(90)
+
+mypen.forward(100)
+
+
---
+++ triangle.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.Screen().bgcolor("Blue")
+#Triangle
+mypen=turtle.Turtle()
+mypen.penup()
+mypen.pendown()
+mypen.forward(100)
+mypen.left(120)
+mypen.forward(100)
+mypen.left(120)
+mypen.forward(100)
+
+turtle.done()
