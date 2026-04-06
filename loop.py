30a55f6691ad7a36ff49eb6c568c0d1e37d33829
Oishii2012 <memahmudaa@gmail.com>
2026-04-06 21:53:59 +0600

    Loop

---
+++ loop.py
@@ -0,0 +1,13 @@
+print('Multiplication table of 999')
+for i in range(1,1001):
+ print('999 x',i,'=',999*i )
+
+
+print('The sum of numbers from 1-10')
+num=1
+sum=0
+while(num<=10):
+ sum=sum+num
+ num=num+1
+
+print('Answer',sum)
