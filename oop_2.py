ae6a5d1d958a94732a2bcd528f41a66aeb498e8f
Oishii2012 <memahmudaa@gmail.com>
2026-07-24 22:12:42 +0600

    OOP - Part -2

---
+++ oop_2.py
@@ -0,0 +1,24 @@
+class Circle():
+    def __init__(self,r):
+        self.radius=r
+
+    def area(self):
+        return 3.14*self.radius*self.radius
+
+    def perimeter(self):
+        return 2*3.14*self.radius
+
+
+print("************************************")
+print("       CIRCLE CALCULATOR")
+print("************************************")
+
+newCircle=Circle(7)
+
+print("Radius of the Circle is",newCircle.radius)
+print("Area of the Circle is",newCircle.area())
+print("Perimeter of the Circle is",newCircle.perimeter())
+
+print("************************************")
+print("       CALCULATION COMPLETE")
+print("************************************")
